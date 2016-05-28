139663493abd0e9a0de8849d8c9ec0584e347006
Sergo <sersoftin@gmail.com>
2016-05-28 22:48:02 +0300

    Добавил тестовую программу для библиотеки

---
+++ ACTestApplication/MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ACTestApplication
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BidIdTextBox = new System.Windows.Forms.TextBox();
+            this.SubmitBidButton = new System.Windows.Forms.Button();
+            this.ProductsCombobox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // BidIdTextBox
+            // 
+            this.BidIdTextBox.Location = new System.Drawing.Point(139, 12);
+            this.BidIdTextBox.Name = "BidIdTextBox";
+            this.BidIdTextBox.ReadOnly = true;
+            this.BidIdTextBox.Size = new System.Drawing.Size(153, 20);
+            this.BidIdTextBox.TabIndex = 0;
+            // 
+            // SubmitBidButton
+            // 
+            this.SubmitBidButton.Location = new System.Drawing.Point(298, 10);
+            this.SubmitBidButton.Name = "SubmitBidButton";
+            this.SubmitBidButton.Size = new System.Drawing.Size(112, 23);
+            this.SubmitBidButton.TabIndex = 1;
+            this.SubmitBidButton.Text = "Отправить заявку";
+            this.SubmitBidButton.UseVisualStyleBackColor = true;
+            // 
+            // ProductsCombobox
+            // 
+            this.ProductsCombobox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ProductsCombobox.FormattingEnabled = true;
+            this.ProductsCombobox.Location = new System.Drawing.Point(12, 12);
+            this.ProductsCombobox.Name = "ProductsCombobox";
+            this.ProductsCombobox.Size = new System.Drawing.Size(121, 21);
+            this.ProductsCombobox.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 45);
+            this.Controls.Add(this.ProductsCombobox);
+            this.Controls.Add(this.SubmitBidButton);
+            this.Controls.Add(this.BidIdTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SubmitApp";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox BidIdTextBox;
+        private System.Windows.Forms.Button SubmitBidButton;
+        private System.Windows.Forms.ComboBox ProductsCombobox;
+    }
+}
+
---
+++ ACTestApplication/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSharpACLib;
+
+namespace ACTestApplication
+{
+    public partial class MainForm : Form
+    {
+        private AccessControl _acc;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var config = new Configuration
+            {
+                BaseApiUrl = "https://acs.sersoftin.xyz/",
+                PublicKeyHash =
+                    new byte[]
+                    {
+                        0x9F, 0x71, 0x41, 0x48, 0x67, 0x18, 0x73, 0xCE, 0x77, 0x1B, 0xE6, 0x14, 0x0C, 0x25, 0x22,
+                        0xB1,
+                        0x4D, 0xDA, 0xF5, 0x12
+                    },
+                ProductId = 1,
+                HashSalt = "TestHash"
+            };
+
+            _acc = new AccessControl(config);
+
+            ProductsCombobox.DataSource = _acc.GetProducts();
+            ProductsCombobox.DisplayMember = "Name";
+            ProductsCombobox.ValueMember = "Id";
+        }
+    }
+}
